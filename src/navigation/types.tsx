2940364e48b77b3e124895503a423faa8c399a0a
Martin Creedy <martin@openskydevelopment.co.uk>
2023-06-28 12:29:45 +0100

    fix: envs

--- src/navigation/types.tsx
+++ src/navigation/types.tsx
@@ -7,10 +7,17 @@ import type { AccountParamList } from './account-navigator';
 import type { OnboardingStackParamList } from './onboarding-navigator';
 import type { UserStackParamList } from './user-navigator';
 
+type CommunityParamList = {
+  Page: { name: string };
+  Chat: { channelId: string };
+  Forum: { topicId: string };
+};
+
 export type RootStackParamList = {
   Onboarding: NavigatorScreenParams<OnboardingStackParamList>;
   User: NavigatorScreenParams<UserStackParamList>;
   Account: NavigatorScreenParams<AccountParamList>;
+  Community: NavigatorScreenParams<CommunityParamList>;
 };
 
 declare global {
